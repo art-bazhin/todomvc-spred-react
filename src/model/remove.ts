36cddf6ab2918c8336de78d5d7d46d9550cbbd65
art-bazhin <bazhin.artyom@yandex.ru>
2022-08-30 19:37:36 +0300

    update deps

--- src/model/remove.ts
+++ src/model/remove.ts
@@ -5,7 +5,7 @@ import { todoIds, todos } from './todos-all';
 export const [removeTodosSignal, removeTodos] = signal<TodoId[]>();
 
 on(removeTodosSignal, (ids) => {
-  todoIds.set(todoIds().filter((id) => ids.indexOf(id) < 0));
+  todoIds.update(todoIds().filter((id) => ids.indexOf(id) < 0));
 
   todos.update((state) => {
     ids.forEach((id) => delete state[id]);
